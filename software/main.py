efb048228f85c94f8f5959a10b7325da8e3d75c6
Andres Sakk <andres.sakk@ut.ee>
2022-10-31 19:19:19 +0200

    add go to ball state

--- software/main.py
+++ software/main.py
@@ -19,6 +19,10 @@ class StateMachine:
         self.currentState = State.FIND_BALL
         self.robot = omniRobot
         self.imageData = 0
+        self.imageWidth = 0
+        self.imageHeight = 0
+        self.lastXSpeed = 0
+        self.lastYSpeed = 0
 
     def setData(self, data):
         self.imageData = data
@@ -28,31 +32,37 @@ class StateMachine:
 
         if self.currentState == State.FIND_BALL:
             self.findBall()
-
         if self.currentState == State.GO_TO_BALL:
             self.goToBall()
-
         if self.currentState == State.ORBIT:
             self.orbit()
-
         if self.currentState == State.THROW:
             self.throw()
-
         if self.currentState == State.MANUAL:
             pass
-
 
     #find any ball
     def findBall(self):
         if len(self.imageData.balls) == 0:
             self.robot.move(0,0,1)
         else:
-            #mine teise state'iq
+            #mine teise state'i
             self.robot.stop()
             self.currentState = State.GO_TO_BALL
 
     def goToBall(self):
         #proportsionaalne liikumine
+        #also positive acceleration is capped
+
+        #yle joonte ei l2he
+        #if detect black + white line and if ball y is smaller then do 180degrees turn?
+        #dunno how to do this, possibly find holes in orange field
+
+        #TODO calibrate this value
+        if ball.size > 300:
+            robot.stop()
+            #self.currentState = State.ORBIT
+
         '''
         x koordinaadist lahutatakse pool pildi laiusest ja normaliseeritakse palli kaugus keskpunktist [-1, 1]
         y kiirus on pöördv6rdeline y koordinaadiga
@@ -63,11 +73,49 @@ class StateMachine:
          /
         /
         '''
-        pass
+        try:
+            self.ballCoords = getLargestBallCoords(self.imageData)
+            self.ballXCoord = self.ballCoords[0]
+            self.ballYCoord = self.ballCoords[1]
+        except:
+            self.currentState = State.FIND_BALL
+            return
 
+        #ball x coord dist from centre [-1, 1], can try different speeds
+        self.normalizedXDistanceFromCenter = (self.ballXCoord - (self.imageWidth / 2)) / self.imageWidth
+        #y is 1 if ball coord 0, value [0, 1]
+        self.normalizedYDistanceFromCenter = (self.imageHeight - self.ballYCoord) / self.imageHeight
+
+        self.xSpeedMultiplier = 1
+        self.ySpeedMultiplier = 2
+        self.rotSpeedMultiplier = 0.3
+
+        #calculate acceleration, also need to calibrate this.....
+        self.robotXSpeed = self.normalizedXDistanceFromCenter * self.xSpeedMultiplier
+        self.robotYSpeed = self.normalizedYDistanceFromCenter * self.ySpeedMultiplier 
+        self.robotRotSpeed = self.normalizedXDistanceFromCenter * self.rotSpeedMultiplier
+
+        self.robot.move(self.robotXSpeed, self.robotYSpeed, self.robotRotSpeed)
+
+
+    def getLargestBallCoords(self, imageData):
+        self.largestBallX = 0
+        self.largestBallY = 0
+        self.largestBallSize = 0
+
+        for ball in imageData.balls: 
+            if ball.size > self.largestBallSize:
+                self.largestBallX = ball.x
+                self.largestBallY = ball.y
+
+        return (self.largestBallX, self.largestBallY) 
+    
+    #1st thing center largest ball???
+    #if i see the hoop, orbit accordingly, else just orbit 
     def orbit(self):
         pass
 
+    #use rear wheel correcting and set correct thrower speed
     def throw(self):
         pass
 
@@ -75,7 +123,6 @@ class StateMachine:
 
 
 # TODO: RUN COLOR CONFIGURATOR
-
 def main():
     debug = True
     # camera instance for realsense cameras
@@ -85,6 +132,8 @@ def main():
     
     omniRobot = motion.OmniMotionRobot()
     stateMachine = StateMachine(omniRobot)
+    stateMachine.imageWidth = cam.rgb_width
+    stateMachine.imageHeight = cam.rgb_height
 
     start = time.time()
     fps = 0
@@ -95,12 +144,20 @@ def main():
         while True:
             # has argument aligned_depth that enables depth frame to color frame alignment. Costs performance
             processedData = processor.process_frame(aligned_depth=False)
+            #updating state machine data
             stateMachine.setData(processedData)
 
             # This is where you add the driving behaviour of your robot. It should be able to filter out
             # objects of interest and calculate the required motion for reaching the objects
-            
-            stateMachine.setState(State.FIND_BALL)
+            if stateMachine.currentState == State.FIND_BALL:
+                stateMachine.setState(State.FIND_BALL)
+            if stateMachine.currentState == State.GO_TO_BALL:
+                stateMachine.setState(State.GO_TO_BALL) 
+            if stateMachine.currentState == State.ORBIT:
+                stateMachine.setState(State.ORBIT)
+            if stateMachine.currentState == State.THROW:
+                stateMachine.setState(State.THROW)
+
 
             frame_cnt +=1
             frame += 1
