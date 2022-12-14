8d87befefa322577928bd8b3a6a60685be304e19
Andres Sakk <andres.sakk@ut.ee>
2022-12-14 18:49:55 +0200

    imgproc

--- software/image_processor.py
+++ software/image_processor.py
@@ -72,6 +72,48 @@ class ImageProcessor():
     def stop(self):
         self.camera.close()
 
+    def find_multiple_matching_pixels(self,color, x, y, image, direction, pixels): 
+            pixel_counter = pixels
+            try:
+                while pixel_counter > 0:
+                    if image[x, y] == color:
+                        pixel_counter -= 1
+                    else:
+                        pixel_counter = 10
+                    if direction == "-y":
+                        y -= 1
+                    if direction == "y":
+                        y += 1
+                    if direction == "-x":
+                        x -= 1
+                    if direction == "x":
+                        x += 1
+                return x, y
+            except:
+                return False
+    
+    def detect_line_between_ball_and_robot(self, x, y,fragmented):
+        rgb = fragmented
+        black = c.Color.BLACK.color.tolist()
+        white = c.Color.WHITE.color.tolist()
+        
+        try:
+            after_dark_cordinates = find_multiple_matching_pixels(black, x, y, rgb, "-y", 10)
+            find_multiple_matching_pixels(white, after_dark_cordinates[0], after_dark_cordinates[1], rgb, "-y", 10)
+            return True
+        except:
+            return False
+        
+    def detect_if_ball_in_black(self, x, y,fragmented):
+        rgb = fragmented
+        black = c.Color.BLACK.color.tolist()
+        try:
+            find_multiple_matching_pixels(black, x, y, rgb, "-x", 30)
+            find_multiple_matching_pixels(black, x, y, rgb, "x", 30)
+            return True
+        except:
+            return False
+
     def analyze_balls(self, t_balls, fragments) -> list:
         #maybe this costs a lot of performance
         #t_balls = cv2.morphologyEx(t_balls, cv2.MORPH_OPEN, self.kernelSmall)
@@ -85,53 +127,6 @@ class ImageProcessor():
 
             # ball filtering logic goes here. Example includes filtering by size and an example how to get pixels from
             # the bottom center of the fram to the ball
-            
-            def find_multiple_matching_pixels(color, x, y, image, direction, pixels): 
-                    how_many_pixels = pixels
-                    pixel_counter = how_many_pixels
-                    try:
-                        while pixel_counter > 0:
-                            if image[x, y] == color:
-                                pixel_counter -= 1
-                            else:
-                                pixel_counter = 10
-                            if direction == "-y":
-                                y -= 1
-                            if direction == "y":
-                                y += 1
-                            if direction == "-x":
-                                x -= 1
-                            if direction == "x":
-                                x += 1
-                        return x, y
-                    except:
-                        return False
-            
-            def detect_line_between_ball_and_robot(x, y):
-                processedData = processor.process_frame()
-                rgb = processedData.color_frame
-                black = c.Color.BLACK.color.tolist()
-                white = c.Color.WHITE.color.tolist()
-                
-                try:
-                    after_dark_cordinates = find_multiple_matching_pixels(black, x, y, rgb, "-y", 10)
-                    find_multiple_matching_pixels(white, after_dark_cordinates[0], after_dark_cordinates[1], rgb, "-y", 10)
-                    return True
-                except:
-                    return False
-                
-            def detect_if_ball_in_black(x, y):
-                processedData = processor.process_frame()
-                rgb = processedData.color_frame
-                black = c.Color.BLACK.color.tolist()
-                try:
-                    find_multiple_matching_pixels(black, x, y, rgb, "-x", 30)
-                    find_multiple_matching_pixels(black, x, y, rgb, "x", 30)
-                    return True
-                except:
-                    return False
-                    
-                    
                 
             size = cv2.contourArea(contour)
 
@@ -152,7 +147,7 @@ class ImageProcessor():
                 cv2.circle(self.debug_frame,(obj_x, obj_y), 10, (0,255,0), 2)
                 
             #ignore noise above the field and balls over the line and balls in black
-            if obj_y > 80 and detect_line_between_ball_and_robot(obj_x, obj_y) == False and detect_if_ball_in_black(obj_x, obj_y) == False:
+            if obj_y > 80 and self.detect_line_between_ball_and_robot(obj_x, obj_y) == False and self.detect_if_ball_in_black(obj_x, obj_y) == False:
                 balls.append(Object(x = obj_x, y = obj_y, size = size, distance = obj_dst, exists = True))
 
         balls.sort(key= lambda x: x.size, reverse = True)
